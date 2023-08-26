b753771012ba95eea996ad9e7dcd30b62f0dd8b7
Keval Panwala <107193741+kevalpanwala27@users.noreply.github.com>
2023-08-27 01:20:00 +0530

    saving progress

--- client/src/components/TeamMessage.jsx
+++ client/src/components/TeamMessage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { MessageTeam, useMessageContext } from 'stream-chat-react';
+import { MessageList, useMessageContext } from 'stream-chat-react';
 
 const TeamMessage = () => {
     const { handleOpenThread, message } = useMessageContext();
 
     return (
-        <MessageTeam
+        <MessageList
             message={{ ...message, user: {}}}
             // handleOpenThread={}
         />
